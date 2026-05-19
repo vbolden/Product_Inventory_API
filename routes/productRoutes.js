a399b9f9db24519aba3a846bcb936f3d2d037d62
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-19 15:43:37 -0400

    implemented put function to update a product and log updated product or error message

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -18,14 +18,28 @@ productRouter.get("/:id", async (req, res) => {
     try {
         const product = await Product.findById(req.params.id);
 
-        res.json(product)
+        res.json(product);
 
     } catch (error) {
         res.status(404).json({ error: error.message });
     }
-})
+});
 
 // UPDATE
+productRouter.put("/:id", async (req, res) => {
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+
+        res.json(updatedProduct)
+
+    } catch (error) {
+        res.status(404).json({ error: error.message });
+    }
+});
 
 // DELETE
 
