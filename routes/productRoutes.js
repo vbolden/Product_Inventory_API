0e239157e97a666ea425cdc499a913599108f74f
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-19 15:08:26 -0400

    implemented post function to create a new product and log product or error message

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,3 +1,22 @@
 const productRouter = require('express').Router();
 const Product = require("../models/Product");
 
+// CREATE
+productRouter.post("/", async (req, res) => {
+    try {
+        const newProduct = await Product.create(req.body);
+
+        res.status(201).json(newProduct);
+
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+// READ SINGLE PRODUCT
+
+// UPDATE
+
+// DELETE
+
+// READ ALL 
