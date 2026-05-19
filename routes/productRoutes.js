133c93c91cef1c5592ee8cfaaba04f658ef17f0e
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-19 15:18:08 -0400

    implemented get function to retrieve a single product and log product or error message

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -14,6 +14,16 @@ productRouter.post("/", async (req, res) => {
 });
 
 // READ SINGLE PRODUCT
+productRouter.get("/:id", async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+
+        res.json(product)
+
+    } catch (error) {
+        res.status(404).json({ error: error.message });
+    }
+})
 
 // UPDATE
 
