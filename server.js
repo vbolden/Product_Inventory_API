e3d268149303e809d8ebf2019e86c8067a89e84f
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-19 16:57:16 -0400

    imported productRouter, added middleware and mounted routes

--- server.js
+++ server.js
@@ -4,5 +4,24 @@ require('dotenv').config();
 const PORT = process.env.PORT;
 
 const connectDB = require("./db/connection");
+const productRouter = require("./routes/productRoutes");
 
+// CONNECT TO DATABASE
 connectDB();
+
+// MIDDLEWARE
+app.use(express.json())
+
+// ROUTES
+// TEST ROUTE
+// app.get("/", (req, res) => {
+//     res.send("Test Route")
+// })
+
+// PRODUCT ROUTES
+app.use("/api/products", productRouter);
+
+// PORT
+app.listen(PORT, () => {
+    console.log(`Server running on PORT: ${PORT}`);
+});
