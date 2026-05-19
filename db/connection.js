ed195f1d2ef64f8636096949674fcd9a99ffffe1
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-19 14:43:40 -0400

    created a function to establish mongoDB connection and handle successful connection and errors

--- db/connection.js
+++ db/connection.js
@@ -1,2 +1,15 @@
 const mongoose = require('mongoose');
 
+require('dotenv').config();
+const uri = process.env.MONGODB_URI;
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(uri);
+        console.log('Successfully connected to database');
+    } catch (error) {
+        console.error('MongoDB connection error: ', error);
+    }
+};
+
+module.exports = connectDB;
