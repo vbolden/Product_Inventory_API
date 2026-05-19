17bcf2386c06b2105202b547e656949f2ad96f1b
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-19 14:45:30 -0400

    imported and called connectDB function

--- server.js
+++ server.js
@@ -2,4 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT;
-const uri = process.env.MONGODB_URI;
+
+const connectDB = require("./db/connection");
+
+connectDB();
